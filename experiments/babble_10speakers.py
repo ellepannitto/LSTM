2f1a025fc0a30f23874c7de7d5fee55edd67bc61
ellepannitto <ellepannitto@gmail.com>
2022-05-10 17:00:57 +0200

    glob debug

--- experiments/babble_10speakers.py
+++ experiments/babble_10speakers.py
@@ -22,7 +22,7 @@ for i in range(1, 11):
 
     seed = random.randint(0,1000)
 
-    model_path = list(glob.glob(_model_dir+"/{}/*.pt".format(n)))[0]
+#    model_path = list(glob.glob(_model_dir+"/{}/*.pt".format(n)))[0]
     print(_model_dir+"/{}/*.pt".format(n))
     print(list(glob.glob(_model_dir+"/{}/*.pt".format(n))))
     input()
